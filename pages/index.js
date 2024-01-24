2e5528bc673687e1391ccccd34f2e94685fbd7ea
itc-n23033 <n23033@std.it-college.ac.jp>
2024-01-24 06:59:31 +0000

    chapter1-7

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,15 @@
 const Home = () => {
-  return <h1>CUBE</h1>
+  return (
+    <>
+      <header>HEADER</header>
+      <main>
+        <div>
+          <h1>CUBE</h1>
+          <p>アウトプットしていくサイト</p>
+        </div>
+      </main>
+      <footer>FOOTER</footer>
+    </>
+  )
 }
 export default Home
