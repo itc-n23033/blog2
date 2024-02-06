7495abba2be2fa62df9b154f1f753db00847d87a
itc-n23033 <n23033@std.it-college.ac.jp>
2024-02-06 06:48:37 +0000

    chp2-5end

---
+++ components/layout.js
@@ -0,0 +1,14 @@
+import Header from 'components/header'
+import Footer from 'components/footer'
+
+function Layout ({ children }) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
